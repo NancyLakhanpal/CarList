1e1f0c468adc0b9d922a9f44db8e66467d8e7be9
Nancy Lakhanpal <nancy.lakhanpal18@gmail.com>
2021-03-16 11:41:33 -0700

    proper implementation of add form and edit form

--- src/ComponentsNla25/CarListNla25/CarListNla25.js
+++ src/ComponentsNla25/CarListNla25/CarListNla25.js
@@ -1,16 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { addCar, deleteCars, editcar, getCars } from "../../ServicesNla25/carServiceNla25";
+import Addform from "../AddForm/AddForm";
+import EditForm from "../EditForm/EditForm";
 
 function CarListNla25()
 {
     const [Cars, setCars] = useState([]);
+    const [newCar, setNewCar] = useState();
+    const [action, setAction] = useState("add");
 
     useEffect(() =>{
         refreshPage()
     },[])
 
     function refreshPage(){
-        
+        getCars()
+        .then(json=>{
+            setCars(json);
+        })
+        .catch(err => {
+            console.error(err)
+        })
     }
+
+    function renderEditItemCar(e){
+        e.preventDefault();
+        let carToUpdate={};
+        Cars.forEach((car) => {
+            if(car.id===e.target.value){
+                carToUpdate=car;
+            }
+        })
+        setNewCar(carToUpdate);
+        setAction("edit");
+    }
+
+    async function deleteItem(e){
+        await deleteCars(e.target.value)
+        refreshPage();
+    }
+
+    function updateCar(e){
+        setNewCar({
+            ...newCar,
+            [e.target.id] : e.target.value
+        })
+    }
+
+    async function editItem(e){
+        let updatedItem = newCar;
+        await editcar(updatedItem)
+        refreshPage();
+        setAction("add");
+    }
+
+    async function addItem(e){
+        await addCar(newCar);
+        refreshPage();
+    }
+
 
     return(
         <div className="row border-dark border rounded">
@@ -28,11 +76,25 @@ function CarListNla25()
 
                 </thead>
                 <tbody>
+                    {Cars.map((car, key) => (
+                        <tr key={key}>
+                            <td>{car.id}</td>
+                            <td>{car.name}</td>
+                            <td>{car.model}</td>
+                            <td><button className="alert-primary alert" value={car.id} onClick={renderEditItemCar}>Edit</button></td>
+                            <td><button className="alert-primary alert" value={car.id} onClick={deleteItem}>Delete</button></td>
+                        </tr>
+                    ))}
                     
                 </tbody>
                 </table>
             </div>
 
+            {(action === "add") ? 
+            <Addform update={updateCar} action={addItem}/> : <EditForm newCarData={newCar} action={editItem} update={updateCar}/>}
+
         </div>
     )
 }
+
+export default CarListNla25;
